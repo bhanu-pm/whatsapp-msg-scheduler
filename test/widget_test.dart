8764ec7dc70bd8cc7a01be633069e251e317b03c
bhanu-pm <bhanuprakashm1108@gmail.com>
2024-12-20 00:59:11 -0700

    working web prototype with some errors

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:whatsapp_msg_scheduler/main.dart';
+import 'package:whatsapp_msg_scheduler/main_original.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
